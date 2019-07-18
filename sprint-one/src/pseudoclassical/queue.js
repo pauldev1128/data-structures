519d8d22a5bb16e10c09469228adbae81096f73e
paul cho <pauldev1128@gmail.com>
2019-07-18 15:30:00 -0700

    stacks: created a count var to keep track; queue: created 2 counts to keep track

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count1 = 0;
+  this.count2 = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.count1] = value;
+  this.count1++
+}
+
+Queue.prototype.dequeue = function(){
+  let shifted = this.storage[this.count2];
+  delete this.storage[this.count2];
+  this.count2++;
+  return shifted;
+}
+
+Queue.prototype.size = function(){
+  return Object.keys(this.storage).length;
+}
 
