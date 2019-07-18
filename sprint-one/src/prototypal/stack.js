ed197b93b6d4a340e9184dbcb23c39dd9f19f8d2
paul cho <pauldev1128@gmail.com>
2019-07-18 15:29:08 -0700

    created a count to add and remove values from the storage edit 2

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,32 +1,3 @@
-// var Stack = function() {
-//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-//   // but try not not reference your old code in writing the new style.
-//   let instance = Object.create(stackMethods);
-//   instance.storage = {};
-//   // instance.count1 = 0;
-//   return instance;
-// };
-
-// var stackMethods = {};
-// stackMethods.push = function(value){
-//   let keys = Object.keys(this.storage);
-//   let nextKey = this.storage[keys] === 0 ? 0 : Math.max.apply(null,keys);
-//   this.storage[nextKey] = value;
-//   // this.storage[count1];
-//   // this.count1++
-// }
-
-// stackMethods.pop = function(value){
-//   let popped = this.storage[stackMethods.size()-1];
-//   delete this.storage[stackMethods.size()-1];
-//   // count1--;
-//   return popped;
-// }
-
-// stackMethods.size = function(value){
-//   return Object.keys(this.storage).length;
-// }
-
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
