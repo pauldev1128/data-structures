04bd39824d07838b3eda310fd36d35b8aab99d23
paul cho <pauldev1128@gmail.com>
2019-07-18 11:30:54 -0700

    similar to the stack instead I created 2 counters to keep track of the keys of the obj

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+    let instance = Object.create(queueMethods);
+    instance.storage = {};
+    instance.count1 = 0;
+    instance.count2 = 0;
+    return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.count1] = value;
+  this.count1++;
+}
 
+queueMethods.dequeue = function(){
+  let shifted = this.storage[this.count2];
+  delete this.storage[this.count2];
+  this.count2++;
+  return shifted;
+}
+
+queueMethods.size = function(){
+  return Object.keys(this.storage).length
+}
