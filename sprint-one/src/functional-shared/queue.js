78078daa3d39c491afaf22a1afe7c24ca09202e6
paul cho <pauldev1128@gmail.com>
2019-07-18 09:49:58 -0700

    for stack used Math.max to keep track of the obj while Queue I created a counter to keep track

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = {};
+  instance.storage = {};
+  instance.count1 = 0;
+  instance.count2 = 0;
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+
+
+var queueMethods = {
+  enqueue: function(val){
+    // var keys = Object.keys(this.storage)
+    // var nextKey = keys.length === 0 ? 0 : Math.max.apply(null,keys)+1;
+    this.storage[this.count1] = val;
+    this.count1++
+  },
+
+  dequeue: function(){
+    let shifted = this.storage[this.count2];
+    delete this.storage[this.count2];
+    this.count2++
+    return shifted;
+  },
+
+  size: function(){
+    return Object.keys(this.storage).length
+  }
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = {};
+  instance.storage = {};
+  extend(instance, stackMethods);
+  return instance;
 };
 
-var stackMethods = {};
+let extend = function(to, from){
+  for (let key in from){
+    to[key] = from[key];
+  }
+};
+
+
+var stackMethods = {
+
+  push: function(value){
+    var keys = Object.keys(this.storage);
+    var nextKey = keys.length === 0 ? 0 : Math.max.apply(null,keys)+1;
+    this.storage[nextKey] = value;
+  },
+
+  pop: function(){
+    if(this.size !== 0){
+    let keys = Object.keys(this.storage);
+    let lastKey = Math.max.apply(null, keys);
+    let popped = this.storage[lastKey];
+    delete this.storage[lastKey];
+    return popped;
+    } 
+  },
+
+  size: function(){
+    return Object.keys(this.storage).length;
+  }
+
+};
 
 
