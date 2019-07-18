d68420d8a3c5da12f2137d5188076a11e578c15a
paul cho <pauldev1128@gmail.com>
2019-07-18 15:31:32 -0700

    stacks: count variable to keep track; queue: 2 count variable to keep track

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count1 = 0;
+    this.count2 = 0;
   }
 
+  enqueue (value){
+    this.storage[this.count1] = value;
+    this.count1++;
+  }
+
+  dequeue (){
+    let shifted = this.storage[this.count2];
+    delete this.storage[this.count2];
+    this.count2++;
+    return shifted;
+  }
+
+  size (){
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push (value){
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop (){
+    let popped = this.storage[this.count-1];
+    delete this.storage[this.count-1];
+    this.count--;
+    return popped;
+  }
+
+  size (){
+    return Object.keys(this.storage).length;
+  }
 }
