31d8f3e7335c0029ea951712d8e4e272e7b2bd86
paul cho <pauldev1128@gmail.com>
2019-07-18 09:55:02 -0700

    created counters to keep track of the objecte and used Object.keys to find the size of the storage

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.count1 = 0;
+  instance.count2 = 0;
+  return instance;
 };
 
 var stackMethods = {};
+stackMethods.push = function(value){
+  let keys = Object.keys(this.storage)
+  storage[this.size()] = value
+}
 
+stackMethods.pop = function(value){
+  
+}
 
+stackMethods.size = function(value){
+  return Object.keys(this.storage).length
+}
