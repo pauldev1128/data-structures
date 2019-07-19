9cb994214b4cb017b720bdb9224d8debec162fdd
paul cho <pauldev1128@gmail.com>
2019-07-19 13:12:16 -0700

    used while loop for contain to check each value

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-  };
+    let node = new Node(value);
+    if(list.head === null){
+      list.head = node;
+      list.tail = list.head;
+    } else {
+      list.tail.next = node;
+      list.tail = list.tail.next;
+    }
+  }
+
 
   list.removeHead = function() {
-  };
+    let remove = list.head;
+    list.head = list.head.next;
+    return remove.value;
+  }
 
   list.contains = function(target) {
+    let currentNode = list.head;
+    while(currentNode){
+      if(currentNode.value === target){
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    };
+    return false;
   };
 
   return list;
