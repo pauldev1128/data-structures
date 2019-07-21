031de6308847d8f60a63f3bacba64041a164caf8
paul cho <pauldev1128@gmail.com>
2019-07-20 22:03:56 -0700

    added some pseudo codes

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,21 @@
 var BinarySearchTree = function(value) {
+  // add node
+
+  // contains
+
+  // depth first log
+
+  // 
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ // 1) max of two children
+ //  2) child right bigger child left smaller
+ // check if is bigger than property 
+
+
+
