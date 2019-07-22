fa4693c48c47d103e1f94f7a6014850505837702
pauldev1128 <pauldev118@gmail.com>
2019-07-21 22:29:43 -0700

    finished tests... fixed collisions by changing if else conditions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,32 +9,43 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if(this._storage[index] === undefined){
     this._storage[index] = [[k,v]];
-  } else if (this._storage[index].length){
+  }
+  if(this._storage[index].length > 0){
     for(let i = 0; i < this._storage[index].length; i++){
       if(this._storage[index][i][0] === k){
         this._storage[index][i][1] = v;
+      } else {
+        this._storage[index].push([k,v])
       }
     }
   }
-  else {
+  if (this._storage[index].length === 0){
     this._storage[index].push([k,v])
-  }
-  
+  };
+  // if(this._storage === undefined){
+  //   this._storage.set(index,[[k,v]]);
+  // } else{
+  //   this._storage.set (index,v);
+  // }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  let storage = this._storage[index];
-  for(let i = 0; i < storage.length; i++){
-    if(storage[i][0] === k){
-      return storage[i][1];
-    } 
+  for(let i = 0; i < this._storage[index].length; i++){
+    if(this._storage[index][i][0] === k){
+      return this._storage[index][i][1];
+    }
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+  for(let i = 0; i < this._storage[index].length; i++){
+   if(this._storage[index][i][0] === k)
+    return this._storage[index].splice(i,1)
+  }
+
 };
 
 
